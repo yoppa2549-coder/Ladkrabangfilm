c49347ec1bd9464090db4c24400340ed164b0336
yoppa2549-coder <yoppa2549@gmail.com>
2025-08-16 16:34:07 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🎬 Welcome to Ladkrabang Film</h1>
+      <p>โปรดักชั่นเฮ้าส์นักศึกษาที่ใหญ่ที่สุด!</p>
+    </main>
+  );
+}
 
